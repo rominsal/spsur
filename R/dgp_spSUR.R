ebd70bf2712875e112227eb114c843802c163842
Román Mínguez <roman.minguez@uclm.es>
2018-11-19 13:37:59 +0100

    Add files via upload

--- R/dgp_spSUR.R
+++ R/dgp_spSUR.R
@@ -4,27 +4,31 @@
 #' @title Data Generating Process spSUR
 #'
 #' @description
-#' Data Generating Process of a spatial SUR. nRxnGxnT with p_{g} (g=1,...,nG) independent variables (all N(0,1)) 
+#' Data Generating Process of a spatial SUR. nRxnGxnT with \deqn{p_{g}} (g=1,...,nG) independent variables (all N(0,1))
 #' with constant term and equal coefficients beta for all independent variables
 #'
 #' @param  Sigma  : Covariance matrix between equations.
-#' @param  Coeff  : A vector 2x1. The first term is the constant and the second is the slope (the same for all equations)
-#' @param  rho    : Level of spatial autocorrelation in lag term. A nGx1 vector rho[g]=level of spatial autocorrelation in equation 'g'. If rho=number,then the same level for all equations.
-#' @param  lambda : Level of spatial autocorrelation in error term. A nGx1 vector lambda[g]=level of spatial autocorrelation in equation 'g'. If rho=number,then the same level for all equations.
-#' @param  p      : A nGx1 vector p_{g}=number of explicative variables (Xs) in equation 'g'. If p_{g}=number the same number of variables in all equations.
+#' @param  Betas  : Vector of beta coefficients including intercept term
+#' @param  Thetas : Vector of delta coefficients for Durbin processes. Default: NULL
+#' @param  durbin : Logical for durbin type processes. Default: FALSE.
+#' @param  lambda : Level of spatial autocorrelation in lag term. A nGx1 vector rho[g]=level of spatial autocorrelation in equation 'g'. If rho=number, then the same level for all equations.
+#' @param  rho    : Level of spatial autocorrelation in error term. A nGx1 vector lambda[g]=level of spatial autocorrelation in equation 'g'. If rho=number, then the same level for all equations.
+#' @param  p      : A nGx1 vector \eqn{p_{g}}=number of covariates (including intercept) in equation 'g'. If \eqn{p_{g}}=number the same number of variables in all equations.
 #' @param  nR     : Number of spatial units.
 #' @param  nT     : Number of time periods.
 #' @param  nG     : Number of equations in SUR model.
 #' @param  W      : A nRxnR spatial weight matrix.
 #'
 #' @return
-#' A list with a matrix Y (nRxnGxnT)x1 with dependent variable and matrix XX (nRxnGxnT)xsum(p)
+#' A list with a matrix Y (nRxnGxnT)x1 with dependent variable and matrix X (nRxnGxnT)xsum(p)
 #'
 #' @details
-#' This function allows to simulate data samples of SUR-SAR; SUR-SEM and SUR-SARAR models. 
+#' This function allows to simulate data samples of SUR-SAR; SUR-SEM and SUR-SARAR models.
 #' It can be used to make Monte Carlo experiments.
-#' 
-#' The model specification for nR individuals, nT cross sections and nG equations, with spatial interaction mechanisms, as follows:\cr
+#'
+#' The model specification for nR individuals, nT cross sections and nG equations, with spatial interaction mechanisms,
+#' as follows:\cr
+#' \cr
 #' SUR-SAR\cr
 #' \deqn{y_{gt}=\lambda_{g} W y_{gt} + X_{gt} \beta_{g} + \epsilon_{gt}} \cr
 #' SUR-SEM\cr
@@ -33,10 +37,12 @@
 #' SUR-SARAR\cr
 #' \deqn{y_{gt}=\lambda_{g} W y_{gt} + X_{gt} \beta_{g} + u_{gt}}
 #' \deqn{u_{gt} = \rho_{g} W u_{gt} + \epsilon_{gt}}
-#' where y_{gt}, u_{gt} and \eqn{\epsilon_{gt}} are (nRx1) vectors; X_{gt} is a matrix of exogenous variables of order (nRxsum(p)); \eqn{\lambda_g} and \eqn{\rho_g} are parametres of spatial dependence; W is the RxR matrix.
+#' where \eqn{y_{gt}}, \eqn{u_{gt}} and \eqn{\epsilon_{gt}} are (nRx1) vectors; \eqn{X_{gt}} is a matrix
+#' of exogenous variables of order (nRxsum(p)); \eqn{\lambda_{g}} and \eqn{\rho_{g}} are
+#'  parametres of spatial dependence; W is the nRxnR matrix.
 
 #' @references
-#' López, F.A., Mur, J., & Angulo, A. (2014). Spatial model selection strategies in a SUR framework. The case of regional productivity in EU. \emph{Annals of Regional Science}, 53(1), 197-220.
+#' López, F.A., Mur, J., and Angulo, A. (2014). Spatial model selection strategies in a SUR framework. The case of regional productivity in EU. \emph{Annals of Regional Science}, 53(1), 197-220.
 #' \cr
 #'
 #' @author
@@ -48,45 +54,55 @@
 #' @seealso
 #' \code{\link{lmtestspsur}},\code{\link{spsurml}}, \code{\link{spsur3sls}}
 #' @examples
-#' 
+#'
 #' ####################################
 #' ######## CROSS SECTION DATA ########
 #' ####################################
-#' 
-#' #### Example 1: DGP SAR model
+#'
+#' #### Example 1: DGP SDM model
 #' nT <- 1 # Number of time periods
 #' nG <- 3 # Number of equations
 #' nR <- 500 # Number of spatial elements
 #' p <- 3 # Number of independent variables
 #' Sigma <- matrix(0.3,ncol=nG,nrow=nG)
-#' diag(Sigma)<-1
-#' Coeff <- c(2,3)
-#' rho <- 0.5 # level of spatial dependence
-#' lambda <- 0.0 # spatial autocorrelation error term = 0
-#' # ramdom coordinates
+#' diag(Sigma) <- 1
+#' Betas <- c(1,2,3,1,-1,0.5,1,-0.5,2)
+#' Thetas <- c(1,-1,0.5,-0.5,1,0)
+#' lambda <- 0.5 # level of spatial dependence
+#' rho <- 0.0 # spatial autocorrelation error term = 0
+#' #  random coordinates
 #' co <- cbind(runif(nR,0,1),runif(nR,0,1))
 #' W <- spdep::nb2mat(spdep::knn2nb(spdep::knearneigh(co,k=5,longlat=F)))
-#' DGP <- dgp_spSUR(Sigma=Sigma,Coeff=Coeff,rho=rho,lambda=lambda,nT=nT,nG=nG,nR=nR,p=p,W=W)
-#' 
+#' DGP <- dgp_spSUR(Sigma=Sigma,Betas=Betas,Thetas=Thetas,durbin=TRUE,
+#'                  rho=rho,lambda=lambda,nT=nT,nG=nG,nR=nR,p=p,W=W)
+#' SDM <- spsurml(W=W,X=DGP$X,Y=DGP$Y,nT=nT,nR=nR,nG=nG,
+#'                p=c(3,3,3),type="sdm")
+#' SAR <- spsurml(W=W,X=DGP$X,Y=DGP$Y,nT=nT,nR=nR,nG=nG,
+#'                p=c(3,3,3),type="sar")
+
 #' ### Example 2: DGP SEM model
 #' nT <- 1 # Number of time periods
 #' nG <- 3 # Number of equations
 #' nR <- 500 # Number of spatial elements
-#' p <- c(1,2,3) # Number of independent variables
+#' p <- c(2,3,4) # Number of independent variables
 #' Sigma <- matrix(0.8,ncol=nG,nrow=nG)
-#' diag(Sigma)<-1
-#' Coeff <- c(2,3)
-#' rho <- 0 # level of spatial dependence = 0
-#' lambda <- c(0.2,0.5,0.8) # spatial autocorrelation error terms for each equation  
-#' # ramdom coordinates
+#' diag(Sigma) <- 1
+#' Betas <- c(1,2,1,2,3,1,2,3,4)
+#' lambda <- 0 # level of spatial dependence = 0
+#' rho <- c(0.2,0.5,0.8) # spatial autocorrelation error terms for each equation
+#' # random coordinates
 #' co <- cbind(runif(nR, 0, 1),runif(nR, 0, 1))
 #' W <- spdep::nb2mat(spdep::knn2nb(spdep::knearneigh(co,k=5,longlat=F)))
-#' DGP <- dgp_spSUR(Sigma=Sigma,Coeff=Coeff,rho=rho,lambda=lambda,nT=nT,nG=nG,nR=nR,p=p,W=W)
-#' 
+#' DGP <- dgp_spSUR(Sigma=Sigma,Betas=Betas,rho=rho,lambda=lambda,nT=nT,nG=nG,nR=nR,p=p,W=W)
+#' SAR <- spsurml(W=W,X=DGP$X,Y=DGP$Y,nT=nT,nR=nR,nG=nG,
+#'                p=c(2,3,4),type="sar")
+#' SEM <- spsurml(W=W,X=DGP$X,Y=DGP$Y,nT=nT,nR=nR,nG=nG,
+#'                p=c(2,3,4),type="sem")
+#'
 #' ####################################
 #' ######## PANEL DATA (nG>1;nT>1) ####
 #' ####################################
-#' 
+#'
 #' ######## Example 3: Equal number of regressors
 #' rho12 <- 0.8; rho13 <- 0; rho23 <- 0.5
 #' sigma11 <- 1; sigma22 <- 2; sigma33 <- 1.5
@@ -96,10 +112,11 @@
 #' Sigma <- matrix(c(sigma11,sigma12,sigma13,sigma12,sigma22,sigma23,sigma13,sigma23,sigma33),nrow=3,ncol=3,byrow=TRUE)
 #' # Check Sigma is positive definite
 #' eigen(Sigma)
-#' 
+#' Betas <- c(1:3,-1:1,3:5)
+#'
 #' # Para nR=3000, nG=3 y nT=5 da problemas al estimar matriz covarianzas en SAR.
-#' # Habría que repasar como evitar algunas inversas en las covarianzas para esos tamaños.
-#' SAR <- spsurml(W=W,X=DGP$XX,Y=DGP$Y,nT=nT,nR=nR,nG=nG,p=c(3,3,3),type="sar")
+#' SAR <- spsurml(W=W,Betas=Betas,X=DGP$X,Y=DGP$Y,nT=nT,nR=nR,nG=nG,
+#'                p=c(3,3,3),type="sar")
 #' summary(SAR)
 
 #' ######## Example 4: With different number of regressors an levels of spatial dependence
@@ -108,77 +125,112 @@
 #' nT <- 5 # Number of time periods
 #' nG <- 2 # Number of equations
 #' nR <- 2000 # Number of spatial elements
-#' p <- c(3,1) # Number of independent variables
+#' p <- c(4,2) # Number of independent variables
+#' Betas <- c(1,-1,2,0.5,2,3)
 #' # ramdom coordinates
 #' co <- cbind(runif(nR, 0, 1),runif(nR, 0, 1))
 #' W <- spdep::nb2mat(spdep::knn2nb(spdep::knearneigh(co,k=5,longlat=F)))
 #'
-#' DGP <- dgp_spSUR(Sigma=Sigma,Coeff=Coeff,rho=rho,lambda=lambda,nT=nT,nG=nG,nR=nR,p=p,W=W)
+#' DGP <- dgp_spSUR(Sigma=Sigma,Betas=Betas,rho=rho,lambda=lambda,nT=nT,nG=nG,nR=nR,p=p,W=W)
 #'
 #' @export
 #'
 
-dgp_spSUR <- function(Sigma=Sigma,Coeff=Coeff,rho=rho,lambda=lambda,nT=nT,nG=nG,nR=nR,p=p,W=W)
+dgp_spSUR <- function(Sigma, nT = 1, nG, nR, Betas,
+                      Thetas = NULL, durbin = FALSE, rho = NULL,
+                      lambda = NULL, p = NULL, W = NULL, X = NULL)
 {
-  W <- Matrix::Matrix(W)
-  if (length(p)==1){
-    p <- matrix(p,nrow = nG,ncol = 1)
-  }
-  if (length(rho)==1){
-    rho <- as.numeric(matrix(rho,nrow = nG,ncol = 1))
-  }
-  if (length(lambda)==1){
-    lambda <- as.numeric(matrix(lambda,nrow = nG,ncol = 1))
-  }
-  
-  XX <- cbind(matrix(1,nR,1),matrix(runif(nR*p[1]),nR,p[1]))
-  
-  for (i in 1:(nG-1)){
-    XX <- Matrix::bdiag(XX,cbind(matrix(1,nR,1),matrix(runif(nR*p[i+1]),nR,p[i+1])))
-  }
-  #XX <- as.matrix(XX)
-  nam <- matrix(0,ncol = p[1]+1)
-  
-  for(i in 1:(p[1]+1)){
-    assign(paste("C", i, sep = ""), i)    
-  }
-  
-  if (nT>1){
-  for (i in 1:(nT-1)){
-    X2 <- cbind(matrix(1,nR,1),matrix(runif(nR*p[1]),nR,p[1]))
+  if(!is.null(W)) W <- Matrix::Matrix(W) else W <- Matrix::Diagonal(nR)
+  if (!is.null(p) & length(p)==1) p <- matrix(p,nrow = nG,ncol = 1)
+  if (is.null(lambda)) lambda <- rep(0,nG)
+  if (is.null(rho)) rho <- rep(0,nG)
+  if (length(lambda)==1) lambda <- as.numeric(matrix(lambda,nrow = nG,ncol = 1))
+  if (length(rho)==1) rho <- as.numeric(matrix(rho,nrow = nG,ncol = 1))
+  if(is.null(X)) {
+    if (is.null(p)) stop("Arguments X and p can not be NULL simultaneously")
+    X <- cbind(matrix(1,nR,1),
+               matrix(runif(nR*(p[1]-1)),nR,(p[1]-1)))
     for (i in 1:(nG-1)){
-      X2 <- Matrix::bdiag(X2,cbind(matrix(1,nR,1),matrix(runif(nR*p[i+1]),nR,p[i+1])))
+      X <- Matrix::bdiag(X,cbind(matrix(1,nR,1),
+                                 matrix(runif(nR*(p[i+1]-1)),nR,(p[i+1]-1))))
     }
-    #X2 <- as.matrix(X2)
-    XX <- rbind(XX,X2)
+    # nam <- matrix(0,ncol = p[1]+1)
+    # for(i in 1:(p[1]+1)){
+    #   assign(paste("C", i, sep = ""), i)
+    # }
+    if (nT>1){
+      for (i in 1:(nT-1)){
+        X2 <- cbind(matrix(1,nR,1),
+                    matrix(runif(nR*(p[1]-1)),
+                           nR,(p[1]-1)))
+        for (i in 1:(nG-1)){
+          X2 <- Matrix::bdiag(X2,cbind(matrix(1,nR,1),
+                                       matrix(runif(nR*(p[i+1]-1)),
+                                              nR,(p[i+1]-1))))
+        }
+        X <- rbind(X,X2)
+      }
+    }
+
+    # Nombro las columnas de X
+    nam <- c(paste0("Intercep_",1),
+             paste(paste0("X",1,"_"), 1:(p[1]-1), sep=""))
+    if (length(p>1)) {
+      for (i in 2:(length(p))){
+        nam <- c(nam,c(paste0("Intercep_",i),
+                       paste(paste0("X",i,"_"), 1:(p[i]-1),
+                             sep="")))
+      }
+    }
+    dimnames(X)[[2]] <- nam
   }
+
+  if(is.null(p)) {
+    if((ncol(X) %% nG) != 0) stop("Argument p need to be set")
+    p <- rep(ncol(X) / nG, nG)
   }
-  
-  #XX <- as.matrix(XX)
-  # Nombro las columnas de XX
-  nam <- c(paste0("Intercep_",1),paste(paste0("X",1,"_"), 1:p[1], sep=""))
-  if (length(p>1)) {
-  for (i in 2:length(p)){
-    nam <- c(nam,c(paste0("Intercep_",i),paste(paste0("X",i,"_"), 1:p[i], sep="")))
-  }
-  }
-  colnames(XX) <- nam
+
   IT <- Matrix::Diagonal(nT)
   IR <- Matrix::Diagonal(nR)
   IG <- Matrix::Diagonal(nG)
-  IGR <- Matrix::Diagonal(nG*nR)  
+  IGR <- Matrix::Diagonal(nG*nR)
+
+  # CAMBIA MATRIZ X Y COEFICIENTES EN EL CASO DURBIN
+  if (durbin) {
+    WX <- (IT %x% IG %x% W) %*% X
+    dimnames(WX)[[2]] <- paste0("W_",colnames(X))
+    Xdurbin <- NULL
+    pdurbin <- p - 1 # Sin intercepto
+    for (i in 1:length(p))
+    {
+      if (i == 1) {
+        Xdurbin <- cbind(X[,1:p[i]],WX[,2:p[i]])
+        Coeff <- c(Betas[1:p[1]],Thetas[1:pdurbin[1]])
+      } else {
+        Xdurbin <- cbind(Xdurbin,
+                         X[,(cumsum(p)[i-1]+1):cumsum(p)[i]],
+                         WX[,(cumsum(p)[i-1]+2):cumsum(p)[i]])
+        # Sin intercepto
+        Coeff <- c(Coeff,
+                   Betas[(cumsum(p)[i-1]+1):cumsum(p)[i]],
+                   Thetas[(cumsum(pdurbin)[i-1]+1):cumsum(pdurbin)[i]])
+      }
+    }
+    #p <- p + (p-1)  # Para el caso sdm cambia el p (ojo Intercepto)
+  }
+
   S <- Sigma
   #S <- (Matrix::Matrix(1,nrow = nG,ncol=nG)-IG)*cor + IG
   OME <- (IT %x% S) %x% IR
   # Factor Cholesky covarianzas
   chol_OME <- Matrix::Cholesky(OME)
-  # factors_chol_OME <- Matrix::expand(chol_OME)
-  # Lchol_OME <- Matrix::t(factors_chol_OME$P) %*% factors_chol_OME$L
+  #factors_chol_OME <- Matrix::expand(chol_OME)
+  #Lchol_OME <- Matrix::t(factors_chol_OME$P) %*% factors_chol_OME$L
   # Uchol_OME <- Matrix::t(factors_chol_OME$L) %*% factors_chol_OME$P
   # Comprobación factor Choleski
-  # pr <- as(Matrix::tcrossprod(Lchol_OME),"dgTMatrix")
-  # all.equal(OME,pr); range(OME-pr)   
-  
+  #pr <- as(Matrix::tcrossprod(Lchol_OME),"dgTMatrix")
+  #all.equal(OME,pr); range(OME-pr)
+
   M <- Matrix::Matrix(0,ncol=1,nrow=nT*nG*nR)
   U <- matrix(sparseMVN::rmvn.sparse(n=1, mu=M, CH=chol_OME,prec=FALSE),ncol=1)
   # Alternativa también rápida pero no utiliza sparse y tiene problemas para gran tamaño
@@ -189,15 +241,22 @@ dgp_spSUR <- function(Sigma=Sigma,Coeff=Coeff,rho=rho,lambda=lambda,nT=nT,nG=nG,
   # Solución: Evitar cálculo inversa matrices
   #IA <- IT %x% Matrix::solve(IGR - diag(rho) %x% W)
   #IB <- IT %x% Matrix::solve(IGR - diag(lambda) %x% W)
-  
-  beta <- as.matrix(c(Coeff[1], Coeff[2]*matrix(1,ncol=p[1],nrow=1)))
-  for (i in 1:(nG-1)){
-    beta <- rbind(beta,as.matrix(c(Coeff[1], Coeff[2]*matrix(1,ncol=p[i+1],nrow=1))))
-  }
-  #Y <- IA %*% (XX %*% beta + IB %*% U)
+
+  # beta <- as.matrix(c(Betas[1], Betas[2]*matrix(1,ncol=p[1],nrow=1)))
+  # for (i in 1:(nG-1)){
+  #   beta <- rbind(beta,as.matrix(c(Betas[1], Betas[2]*matrix(1,ncol=p[i+1],nrow=1))))
+  # }
+  #Y <- IA %*% (X %*% beta + IB %*% U)
   # Con este código evitamos inversas
-  IBU <- Matrix::solve(IT %x% (IGR - diag(lambda) %x% W),U)
-  Y <- Matrix::solve(IT %x% (IGR - diag(rho) %x% W) , (XX %*% beta + IBU))
-  
-  results <- list(XX = as.matrix(XX), Y = as.matrix(Y))
+  IBU <- Matrix::solve(IT %x% (IGR - diag(rho) %x% W),U)
+  if (durbin) {
+    Y <- Matrix::solve(IT %x% (IGR - diag(lambda) %x% W) ,
+                       (Xdurbin %*% Coeff + IBU))
+  } else {
+    Y <- Matrix::solve(IT %x% (IGR - diag(lambda) %x% W) ,
+                       (X %*% Betas + IBU))
+  }
+
+  results <- list(X = as.matrix(X), Y = as.matrix(Y))
 }
+
--- R/spSUR-package.R
+++ R/spSUR-package.R
@@ -1,11 +1,15 @@
+#'spSUR
+#'
+#' @docType package
 #' @name spSUR-package
 #' @rdname spSUR-package
 #'
 #' @title Spatial Seemingly Unrelated Regression Models
 #'
 #' @description
-#' A collection of functions to test and estimate Spatial Seemingly Unrelated Regression (SUR) models by maximum
-#' likelihood and three-stage least square the ussual SUR especifications with spatial effects
+#' A collection of functions to test and estimate Spatial Seemingly Unrelated Regression (SUR)
+#' models by maximum likelihood and three-stage least square the usual
+#' SUR especifications with spatial effects
 #' (SUR-SLX; SUR-SAR; SUR-SEM; SUR-SDM; SUR-SDEM and SUR-SARAR) and non spatial SUR model (SUR-SIM)\cr
 #'
 #' @details
@@ -27,7 +31,7 @@
 #' \cr
 #' @section 2.1. Maximum likelihood estimation:
 #' Maximum likelihood estimation for differents spatial SUR models using \code{\link{spsurml}}
-#' . The models are:
+#' . The models are:\cr
 #' SUR-SIM: SUR model without spatial effects\cr
 #' \deqn{y_{gt} = y_{gt} + X_{gt} \beta_{g} + \epsilon_{gt}}
 #' SUR-SLX: \cr
@@ -42,7 +46,7 @@
 #' \deqn{y_{gt} = X_{gt} \beta_{g} + WX_{gt} \theta_{g} + u_{gt} ; u_{gt} = \rho_{g} W u_{gt} + \epsilon_{gt}}
 #' SUR-SARAR: Spatial autoregressive model with spatial autoregressive error term\cr
 #' \deqn{y_{gt} = \lambda_{g} Wy_{gt} + X_{gt} \beta_{g} + u_{gt} ; u_{gt} = \rho_{g} W u_{gt} + \epsilon_{gt}}
-#' where y_{gt}, u_{gt} and \eqn{\epsilon_{gt}} are (nRx1) vectors; X_{gt} is a matrix of exogenous variables of
+#' where \eqn{\y_{gt}}, u_{gt} and \eqn{\epsilon_{gt}} are (nRx1) vectors; \eqn{\X_{gt}} is a matrix of exogenous variables of
 #' order (nRxsum(p)); \eqn{\lambda_g} and \eqn{\rho_g} are parametres of spatial dependence; W is a nRxnR matrix of spatial interactions.
 #' \cr
 #' @section 2.2 Three-Stage Least Squate estimation:
@@ -53,38 +57,68 @@
 #'
 #' @section 3.1 Marginal test:
 #' The Marginal Multiplier tests are used to test for no correlation in one part of the model allowing for spatial correlation in the other.
-#' The LM(LM(\eqn{\rho}|\eqn{\lambda})) is the test for sustantive spatial autocorrelation in a model with spatial autocorrelation
-#' in error term (SUR-SEM; SUR-SDM;SUR-SDEM).
-#' The LM(\eqn{\lambda}|\eqn{\rho}) is the test for spatial error correlation in a model with sustanative spatial correlation (SUR-SAR).\cr
+#' The LM(\eqn{\lambda}|\eqn{\rho}) is the test for sustantive spatial autocorrelation in a model with spatial autocorrelation
+#' in error term (SUR-SEM; SUR-SDEM).
+#' The LM(\eqn{\rho}|\eqn{\lambda}) is the test for spatial error correlation in a model
+#' with sustanative spatial correlation (SUR-SAR; SUR-SDM).\cr
 #'
 #' @section 3.2 Coefficients stability:
 #' The Walt tests of coefficent stability (or equity) could be calculate for beta coefficients \code{\link{wald_betas}} or
-#' spatial autocorrelation coefficients \code{\link{wald_deltas}}
+#' for spatial autocorrelation coefficients \code{\link{wald_deltas}}
 #'
 #' @section 4. Marginal effects:
 #' The marginal effects \code{\link{impacts}} of spatial autoregressive models (SAR; SDM; SARAR) has been calculated
 #' following the propose of LeSage and Pace (2009)
 #'
-#' @section Dataset:
-#' This package comes with three different datasets: spc; NAT; Unemployment
+#' @section 5. Additional funtionalities:
+#' A Data Generating Process of a spatial SUR models is avalible using the function \code{\link{dgp_spSUR}}.
 #'
+#' @section Dataset:
+#' This package comes with four different datasets: spc; NAT; Italian Unemployment\cr
+#' The spc (Spatial Phillips-Curve) is a classical data set from Anselin (1988, p. 203)\cr
+#' The NAT data set is come from
+#' \url{https://geodacenter.github.io/data-and-lab/ncovr/}
+#' Homicides and selected socio-economic characteristics for continental U.S. counties.
+#' Data for four decennial census years: 1960, 1970, 1980 and 1990.\cr
+#' A data set 'Sar' obtain with the function dgp_spSUR to imput data as matrices (alternative to use the \code{\link[Formula]{Formula}} package)
 #' @references
 #'
-#' Breusch T, Pagan A (1980) The Lagrange multiplier test and its applications to model specification in
-#'  econometrics. \emph{Rev Econ Stud} 47: 239-254\cr
-#'  \cr
-#' Mur, J., Lopez, F., and Herrera, M. (2010). Testing for spatial effects in seemingly unrelated regressions. \emph{Spatial Economic Analysis}, 5(4), 399-440.
+#' Breusch T, Pagan A (1980). The Lagrange multiplier test and its applications to model specification in
+#'  econometrics. \emph{Review of Economic Studies} 47: 239-254
 #' \cr
 #' \cr
-#' Lopez, F.A., Mur, J., and Angulo, A. (2014). Spatial model selection strategies in a SUR framework. The case of regional productivity in EU. \emph{Annals of Regional Science}, 53(1), 197-220.
+#' LeSage, J., and Pace, R. K. (2009). \emph{Introduction to spatial econometrics}. Chapman and Hall/CRC.
 #' \cr
 #' \cr
-#' LeSage, J., and Pace, R. K. (2009). Introduction to spatial econometrics. Chapman and Hall/CRC.
-#'
+#' Lopez, F.A., Mur, J., and Angulo, A. (2014). Spatial model selection strategies in a SUR framework.
+#' The case of regional productivity in EU. \emph{Annals of Regional Science}, 53(1), 197-220.
+#' \cr
+#' \cr
+#' López, F.A., Martínez-Ortiz, P.J., and Cegarra-Navarro, J.G. (2017). Spatial spillovers in public
+#' expenditure on a municipal level in Spain. \emph{Annals of Regional Science}, 58(1), 39-65.
+#' \cr
+#' \cr
+#' Mur, J., Lopez, F., and Herrera, M. (2010). Testing for spatial effects in seemingly
+#' unrelated regressions. \emph{Spatial Economic Analysis}, 5(4), 399-440.
+#' \cr
 #' @author
 #'   \tabular{ll}{
 #'   Fernando López  \tab \email{fernando.lopez@upct.es} \cr
 #'   Román Mínguez  \tab \email{roman.minguez@uclm.es} \cr
 #'   Jesús Mur  \tab \email{jmur@unizar.es} \cr
 #'   }
+#'
+#' @importFrom Formula Formula model.part
+#' @importFrom MASS ginv
+#' @importFrom Matrix bdiag crossprod Diagonal Matrix solve t
+#' @importFrom methods as
+#' @importFrom minqa bobyqa
+#' @importFrom numDeriv hessian
+#' @importFrom sparseMVN rmvn.sparse
+#' @importFrom spdep knearneigh knn2nb nb2mat
+#' @importFrom stats cor cov optim pchisq pnorm pt qnorm rnorm runif
+#' @importFrom stats coefficients fitted lm residuals printCoefmat
+#' @importFrom stats model.frame model.matrix terms
+#'
+#' @exportPattern "^[[:alpha:]]+"
 NULL
